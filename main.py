81f63688cfde16bdc6cf4cd6775d3c9706d7af46
rt4bc-mac-mini <bochao.me@gmail.com>
2021-10-08 22:34:55 +0800

    update local config

--- main.py
+++ main.py
@@ -2,5 +2,37 @@
 
 ######################################################
 # python code template
-# pyside6 base application
+# pyside6 basic application with css decoration
 ######################################################
+
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.button = QtWidgets.QPushButton("Click Me!")
+        self.text = QtWidgets.QLabel(
+            "Hello World!", alignment=QtCore.Qt.AlignCenter
+        )
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.click)
+
+    @QtCore.Slot()
+    def click(self):
+        print("Clicked!")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
